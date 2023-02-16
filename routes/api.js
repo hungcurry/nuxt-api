1cee170d1a1bca753a787e4887a37d0780989971
hungcurry <ooopp42@gmail.com>
2023-02-16 23:02:47 +0800

    rdit cart API

--- routes/api.js
+++ routes/api.js
@@ -14,66 +14,7 @@ router.get('/banners', function(req, res, next) {
   })
   res.end();
 });
-// ... post ...
-// ===================
-router.post('/banners', function(req, res, next) {
-  const banner = req.body;
-  KvData.push({
-    id : new Date().getTime(),
-    ...banner,
-  })
 
-  res.status(200);
-  res.send({
-    success : true,
-    data :KvData,
-  })
-  res.end();
-});
-// ... patch ...
-// ===================
-router.patch('/banners', function(req, res, next) {
-  const id = req.body.id;
-  const quantity = req.body.quantity;
-  KvData.forEach((banner , key)=> {
-    if ( banner.id == id){
-      banner.quantity = quantity;
-    }
-  })
-
-  res.status(200);
-  res.send({
-    success : true,
-    data :KvData,
-  })
-  res.end();
-});
-// ... delete ...
-// ===================
-// http://127.0.0.1:5000/api/banners/:id
-router.delete('/banners/:id', function(req, res, next) {
-  const id = req.params.id;
-  KvData.forEach((banner , key)=> {
-    if ( banner.id == id){
-      KvData.splice(key , 1)
-    }
-  })
-  res.status(200);
-  res.send({
-    success : true,
-    data :KvData,
-  })
-  res.end();
-});
-router.delete('/banners', function(req, res, next) {
-  KvData.length = 0;
-  res.status(200);
-  res.send({
-    success : true,
-    data :KvData,
-  })
-  res.end();
-});
 
 // ============================= CartData ===================================
 // ... get ...
